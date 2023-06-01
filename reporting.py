ca4494bd03ccf803ffcd651e8de7514c80e7fe8d
dsanderson <dsa@engora.tech>
2023-05-31 23:29:07 -0400

    Adding README

--- reporting.py
+++ reporting.py
@@ -5,7 +5,7 @@ import json
 
 def report(query, seeds, scores, name, n_results=50):
     folder = REPORTS/name
-    folder.mkdir(parents=True, exists_ok=True)
+    folder.mkdir(parents=True, exist_ok=True)
     with open(folder/'score.json', 'w') as f:
          json.dump(scores, f, indent=2)
     with open(folder/'report.txt', 'w') as f:
